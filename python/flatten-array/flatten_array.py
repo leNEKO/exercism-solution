97823e50f3b1f53eeebbe7ceac141688a2336949
leNEKO <le.neko@gmail.com>
2018-06-18 13:48:42 +0200

    more

--- python/flatten-array/flatten_array.py
+++ python/flatten-array/flatten_array.py
@@ -1,2 +1,10 @@
+# Without duck typing
 def flatten(iterable):
-    pass
+    r = []
+    # passes the tests but will work only with this list of iterable types
+    if type(iterable) in [list, tuple, set]:
+        for i in iterable:
+            r += flatten(i)
+    elif iterable is not None:
+        r.append(iterable)
+    return r
