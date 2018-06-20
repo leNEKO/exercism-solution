5a4c68d8262fc3554cf4e0487008ac434cb9dd29
neko <le.neko@gmail.com>
2018-06-20 15:32:25 +0200

    Upz

--- javascript/prime-factors/prime-factors.js
+++ javascript/prime-factors/prime-factors.js
@@ -1,39 +1,17 @@
+// Ah, ok, "for" needs to be a static method, my bad ...
 class primeFactors {
-    for(i) {
-        for (p of this.sieve(i)) {
-        }
-    }
-
-    divmod(i, d) {
-        q = Math.floor(i / d);
-        r = i % d;
-    }
-
-    sieve(limit) {
-        var nums = [...Array(limit + 1).keys()];
-        nums[1] = 0;
-        for (var i in nums) {
-            if (i != 0) {
-                console.log(limit, i);
-                // for (var y = Math.pow(i, 2); y < limit; y += parseInt(i)) {
-                //     console.log(y);
-                //     nums[y] = 0;
-                // }
+    static for(limit) {
+        var primes = [];
+        var i = 2;
+        while (limit >= i) {
+            while (limit % i === 0) {
+                primes.push(i);
+                limit /= i;
             }
+            i++;
         }
-        console.log(nums);
-
-        // nums[1] = 0
-        // for i in nums:
-        //     if i:
-        //         for y in range(i ** 2, limit, i):
-        //             nums[y] = 0
-        // return [i for i in nums if i > 0]
+        return primes;
     }
 }
 
-if (require.main === module) {
-    p = new primeFactors();
-    p.sieve(100);
-}
 module.exports = primeFactors;
