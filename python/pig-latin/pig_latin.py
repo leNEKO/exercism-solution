9481f5ef840cc30cb1bd796126156d3beb181571
neko <le.neko@gmail.com>
2018-07-03 17:52:07 +0200

    python piglatin

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -1,2 +1,19 @@
+import re
+
+VOYELLE = "aeiouy"
+CONSONNE = "bcdfghjklmnpqrstvwxz"
+
+
 def translate(text):
-    pass
+    # rules 2+3+4
+    pattern = "^(squ|qu|y|[" + CONSONNE + "]+)([" + VOYELLE + "].*)$"
+    words = []
+    for word in text.split(" "):
+        mc = re.search(pattern, word)
+        if mc and not re.search(r"ay$", word):
+            pig_word = mc.group(2) + mc.group(1) + "ay"
+        else:
+            # rules 1
+            pig_word = word + "ay"
+        words.append(pig_word)
+    return " ".join(words)
---
+++ python/simple-cipher/simple_cipher.py
@@ -0,0 +1,25 @@
+class Cipher(object):
+    def __init__(self, key=None):
+        pass
+
+    def codec(str, dir=1):
+        encoded = ""
+        for i in range(0, len(str)):
+            c = str[i]
+            # str char
+            k = this.key[i % this.key.length]
+            // key char
+            var pos = parseInt(by_keys[c])
+            var offset = parseInt(by_keys[k])
+            var npos = mod(pos + offset * dir, alpha.length)
+            var nc = alpha[npos]
+            encoded += nc
+        }
+        return encoded
+    }
+
+    def encode(self, text):
+        pass
+
+    def decode(self, text):
+        pass
