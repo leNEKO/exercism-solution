2039458a2dd3b3ccad75c8ea5f0187eb5508235f
leNEKO <le.neko@gmail.com>
2018-07-07 21:39:04 +0200

    python more

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -11,16 +11,25 @@ def classify(number):
 
     # it seems that if first bit == 1 of tha
     aliquot_sum = 1
+    l = []
     # greatest divisor cannot be greater than half the value
+    limit = int(number ** 0.5)+1
     for i in range(2, int(number ** 0.5)+1):
         if number % i == 0:
             aliquot_sum += i
-            if not i == number//i:
-                aliquot_sum += number//i
-            if aliquot_sum > number:
-                return "abundant"  # substantial optimization :|
+            if not i == number/i:
+                aliquot_sum += number/i
 
+    if aliquot_sum > number:
+        return "abundant"  # substantial optimization :|
     if aliquot_sum == number:
         return "perfect"
-
     return "deficient"
+
+
+def main():
+    classify(64)
+
+
+if __name__ == '__main__':
+    main()
