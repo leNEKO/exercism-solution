25cc0c7dcacb5dd358c5f4f43dfab3d5f5f93196
leNEKO <le.neko@gmail.com>
2018-07-17 13:46:15 +0200

    ceci

--- python/binary-search/binary_search.py
+++ python/binary-search/binary_search.py
@@ -1,2 +1,31 @@
-def binary_search(list_of_numbers, number):
-    pass
+def binary_search(l, n):
+    ll = len(l)  # list length
+    h = ll // 2  # half length
+    o = h  # offset
+
+    if l == []:
+        raise ValueError("Empty array")
+
+    if (l[0] <= n <= l[-1]) == False:
+        raise ValueError("Out of bound")
+
+    while True:
+        print(f"search {n} at {o} found {l[o]}")
+        if l[o] == n:
+            print("WIN!")
+            return o
+        h //= 2
+        o += h if l[o] <= n else -h
+        print(f"next offset {o}")
+        if h == 0:
+            raise ValueError("Not Found")
+
+
+def main():
+    print(
+        binary_search([1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377], 21)
+    )
+
+
+if __name__ == '__main__':
+    main()
