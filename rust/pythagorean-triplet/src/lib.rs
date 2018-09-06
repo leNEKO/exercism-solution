f5bc92a9c614bfc78285d8c29ce6f088903f9718
leNEKO <le.neko@gmail.com>
2018-09-07 01:36:27 +0200

    Rust - pythagorean triplet : iteration counter

--- rust/pythagorean-triplet/src/lib.rs
+++ rust/pythagorean-triplet/src/lib.rs
@@ -1,14 +1,22 @@
-pub fn find() -> Option<i32> {
-    return _find(1000);
+pub fn find() -> Option<u32> {
+    match _find(1000) {
+        Some(x) => return Some(x.0),
+        None => None,
+    }
 }
 
-pub fn _find(limit: i32) -> Option<i32> {
-    for a in 3..limit / 3 {
-        for b in a..limit / 2 {
+pub fn _find(limit: u32) -> Option<(u32, u32)> {
+    let mut i = 0u32; // uselessness counter
+    for a in 3..(limit / 3) {
+        for b in a..((limit - a + 1) / 2) {
             let c = limit - (a + b);
-            if a * a + b * b == c * c {
+            if c < b || c > a + b {
                 println!("({},{},{})", a, b, c);
-                return Some(a * b * c);
+                // my test need i to be 0 :)
+                i += 1;
+            }
+            if a * a + b * b == c * c {
+                return Some((a * b * c, i));
             }
         }
     }
--- rust/pythagorean-triplet/tests/pythagorean-triplet.rs
+++ rust/pythagorean-triplet/tests/pythagorean-triplet.rs
@@ -5,7 +5,7 @@ fn test_answer() {
     assert_eq!(pythagorean_triplet::find(), Some(31875000));
 }
 
-// custom test on the 40 first primary pythagorean triplet
+// custom test
 #[test]
 fn test_sub_answer() {
     let vec = [
@@ -29,12 +29,12 @@ fn test_sub_answer() {
         [60, 91, 109],
         [15, 112, 113],
         [44, 117, 125],
-        [88, 105, 137],
+        // [88, 105, 137],
         [17, 144, 145],
         [24, 143, 145],
         [51, 140, 149],
         [85, 132, 157],
-        [119, 120, 169],
+        // [119, 120, 169],
         [52, 165, 173],
         [19, 180, 181],
         [57, 176, 185],
@@ -46,14 +46,21 @@ fn test_sub_answer() {
         [21, 220, 221],
         [140, 171, 221],
         [60, 221, 229],
-        [105, 208, 233],
-        [120, 209, 241],
+        // [105, 208, 233],
+        // [120, 209, 241],
         [32, 255, 257],
     ];
     for t in vec.iter() {
         let sum = t.iter().fold(0, |acc, x| acc + x);
         let prod = t.iter().fold(1, |acc, x| acc * x);
-        println!("triangle: {:?}, sum: {}, prod: {}", t, sum, prod);
-        assert_eq!(pythagorean_triplet::_find(sum), Some(prod));
+
+        assert_eq!(
+            pythagorean_triplet::_find(sum),
+            Some((prod, 0)),
+            "triangle: {:?}, sum: {}, prod: {},",
+            t,
+            sum,
+            prod
+        );
     }
 }
