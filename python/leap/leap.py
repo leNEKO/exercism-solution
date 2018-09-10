a823ea981cccdd471f11739d91964fabd8ccd4a4
leNEKO <le.neko@gmail.com>
2018-09-10 15:27:46 +0200

    leap: atomical

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,6 +1,2 @@
 def is_leap_year(year):
-    mod4 = (year % 4) == 0
-    mod100 = (year % 100) != 0
-    mod400 = (year % 400) == 0
-    is_leap = mod4 and (mod100 or mod400)
-    return is_leap
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
