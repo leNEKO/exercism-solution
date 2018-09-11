5bdc6a7c3055222157a80804a01ee2ed834cdd05
leNEKO <le.neko@gmail.com>
2018-09-12 00:43:56 +0200

    grade school : db -> _db

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -3,16 +3,16 @@ from collections import defaultdict
 
 class School(object):
     def __init__(self):
-        self.db = defaultdict(set)
+        self._db = defaultdict(set)
 
     def grade(self, n):
-        return list(self.db[n]) or []
+        return sorted(list(self._db[n])) or []
 
     def add_student(self, name, grade):
-        self.db[grade].add(name)
+        self._db[grade].add(name)
 
     def roster(self):
         return [name
-                for k, v in sorted(self.db.items())
+                for k, v in sorted(self._db.items())
                 for name in sorted(v)
                 ]
