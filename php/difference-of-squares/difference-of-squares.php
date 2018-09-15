60de37036d6e4f749ecea6078bdaf9aa54cabb02
leNEKO <le.neko@gmail.com>
2018-09-16 00:44:09 +0200

    difference of squares: return types

--- php/difference-of-squares/difference-of-squares.php
+++ php/difference-of-squares/difference-of-squares.php
@@ -3,17 +3,20 @@
 Mathematically clever solution thanks to http://exercism.io/submissions/53fd3e0ec7e64a58ac9e8671455e5e25
 https://math.stackexchange.com/questions/2260/proof-for-formula-for-sum-of-sequence-123-ldotsn
 https://www.youtube.com/watch?v=drguFeiCMZw
-*/
-function squareOfSums(int $n){
+ */
+function squareOfSums(int $n): int
+{
     // ((N * (N + 1))/2 ) ^ 2
     return (($n * ($n + 1)) / 2) ** 2;
 }
 
-function sumOfSquares(int $n){
+function sumOfSquares(int $n): int
+{
     // (N * (N + 1) * (2N + 1)) / 6
-    return ($n * ($n +1) * ($n * 2 + 1)) / 6;
+    return ($n * ($n + 1) * ($n * 2 + 1)) / 6;
 }
 
-function difference(int $n){
+function difference(int $n): int
+{
     return squareOfSums($n) - sumOfSquares($n);
 }
