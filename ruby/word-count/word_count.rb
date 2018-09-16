5461dcf40e03c0f6315c77dcbd5153008d11b3ec
leNEKO <le.neko@gmail.com>
2018-09-16 20:38:07 +0200

    word cound: first commit

---
+++ ruby/word-count/word_count.rb
@@ -0,0 +1,13 @@
+class Phrase
+    def initialize(str)
+        # normalizing input
+        # lowercase + remove quotes from quoted words
+        @str = str.downcase.gsub(/['"](\w+)['"]/,'\1')
+    end
+
+    def word_count
+        dict = Hash.new(0)
+        @str.scan(/[\w']+/){ |w| dict[w] += 1 }
+        dict
+    end
+end
