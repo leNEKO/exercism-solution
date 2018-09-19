c7ab527c6f6d37e81807c4e37cea4509d7cbdf48
leNEKO <le.neko@gmail.com>
2018-09-19 11:37:40 +0200

    scrabble-score: proper switch implementation

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,6 +1,9 @@
 package scrabble
 
-import "strings"
+import (
+	s "strings"
+	u "unicode"
+)
 
 // ScoreGroup : char -> score
 var ScoreGroup = map[string]int{
@@ -12,18 +15,55 @@ var ScoreGroup = map[string]int{
 	"jx":         8,
 	"qz":         10,
 }
+var runeScore = ScoreMap(ScoreGroup)
 
-// Score : calculate scrabble score from word
-func Score(word string) int {
-	m := ScoreMap(ScoreGroup)
+// Score - calculate scrabble score from word
+var Score = ScoreSwitch
+
+// ScoreSwitch (with hardcoded switchs instead of a map)
+func ScoreSwitch(word string) int {
 	score := 0
-	for _, r := range strings.ToLower(word) {
-		score += m[r]
+	for _, r := range word {
+
+		switch u.ToLower(r) {
+		case 'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't':
+			score += 1
+		case 'd', 'g':
+			score += 2
+		case 'b', 'c', 'm', 'p':
+			score += 3
+		case 'f', 'h', 'v', 'w', 'y':
+			score += 4
+		case 'k':
+			score += 5
+		case 'j', 'x':
+			score += 8
+		case 'q', 'z':
+			score += 10
+		}
 	}
 	return score
 }
 
-// ScoreMap : convert string -> int map to rune -> int map
+// ScoreLowerByRune (convert to lower rune by rune)
+func ScoreLowerByRune(word string) int {
+	score := 0
+	for _, r := range word {
+		score += runeScore[u.ToLower(r)]
+	}
+	return score
+}
+
+// ScoreLowerByword (convert to lower whole string)
+func ScoreLowerByword(word string) int {
+	score := 0
+	for _, r := range s.ToLower(word) {
+		score += runeScore[r]
+	}
+	return score
+}
+
+// ScoreMap - convert string -> int map to rune -> int map
 func ScoreMap(m map[string]int) map[rune]int {
 	var r = map[rune]int{}
 	for group, v := range m {
