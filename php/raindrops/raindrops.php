fc0871b37805562d0d6f7bc9be59bef62ea7bbe9
leNEKO <le.neko@gmail.com>
2018-09-20 20:40:42 +0200

    raindrops: cleaning

--- php/raindrops/raindrops.php
+++ php/raindrops/raindrops.php
@@ -1,9 +1,10 @@
 <?php
-function raindrops(int $i): string{
+function raindrops(int $i): string
+{
     $r = "";
     $r .= ($i % 3 == 0) ? "Pling" : "";
     $r .= ($i % 5 == 0) ? "Plang" : "";
     $r .= ($i % 7 == 0) ? "Plong" : "";
-    $r = $r ?: (string)$i;
+    $r = $r ?: (string) $i;
     return $r;
 }
