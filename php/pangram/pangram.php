800004ab83a9187e02ca711eeb81010a634a97c9
leNEKO <le.neko@gmail.com>
2018-09-20 16:07:31 +0200

    isPangram: preg_match_all() shortcut

--- php/pangram/pangram.php
+++ php/pangram/pangram.php
@@ -1,19 +1,9 @@
 <?php
-setlocale(LC_ALL,'fr_FR.UTF-8'); // my french
-
-function isPangram(string $str): bool{
-    // normalize
-    $trimmed = trim($str);
-
-    $ascii = iconv("UTF-8", "ASCII//TRANSLIT", $trimmed); // bonus
-    $lower = strtolower($ascii);
-
-    // unique letters
-    $letters = array_unique(
-        str_split(
-            preg_replace("/[^a-z]/", "", $lower)
-        )
-    );
+function isPangram(string $str): bool
+{
+    $lower = strtolower($str);
+    preg_match_all('/[a-z]/', $lower, $m);
+    $letters = array_unique($m[0]);
     sort($letters);
-    return $trimmed && $letters === range("a","z");
+    return $letters === range("a", "z");
 }
