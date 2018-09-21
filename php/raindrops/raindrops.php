958e99d135d5aa9388a1d21dd409adf4d8679a62
leNEKO <le.neko@gmail.com>
2018-09-21 17:26:23 +0200

    raindrops: less ugly

--- php/raindrops/raindrops.php
+++ php/raindrops/raindrops.php
@@ -2,9 +2,21 @@
 function raindrops(int $i): string
 {
     $r = "";
-    $r .= ($i % 3 == 0) ? "Pling" : "";
-    $r .= ($i % 5 == 0) ? "Plang" : "";
-    $r .= ($i % 7 == 0) ? "Plong" : "";
-    $r = $r ?: (string) $i;
+    if ($i % 3 == 0) {
+        $r .= "Pling";
+    }
+
+    if ($i % 5 == 0) {
+        $r .= "Plang";
+    }
+
+    if ($i % 7 == 0) {
+        $r .= "Plong";
+    }
+
+    if ($r == "") {
+        $r = (string) $i;
+    }
+
     return $r;
 }
--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,10 +1,16 @@
-# Raindrops : integer to plingplong transformizer
+# Raindrops : slighty better at doing the sounds
 class Raindrops
+  PRIME_TO_SOUND = {
+    3 => 'Pling',
+    5 => 'Plang',
+    7 => 'Plong'
+  }.freeze
+
   def self.convert(int)
-    r = ''
-    r += (int % 3).zero? ? 'Pling' : ''
-    r += (int % 5).zero? ? 'Plang' : ''
-    r += (int % 7).zero? ? 'Plong' : ''
-    r != '' ? r : String(int)
+    sounds = PRIME_TO_SOUND
+             .select { |prime, _| (int % prime).zero? }
+             .values
+
+    sounds.any? ? sounds.join : int.to_s
   end
 end
