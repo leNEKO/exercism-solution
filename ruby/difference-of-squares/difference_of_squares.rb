3d999c08fc2c8715234ae0e58a7f5b5b551b9d6d
leNEKO <le.neko@gmail.com>
2018-09-29 09:59:30 +0200

    difference of squares: full names + unecessary parenthesis fix

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,17 +1,17 @@
 # Squares: with the math tricks
 class Squares
-  def initialize(int)
-    @int = int
+  def initialize(integer)
+    @integer = integer
   end
 
   def square_of_sum
     # ((N * (N + 1)) / 2) ^ 2
-    ((@int * (@int + 1)) / 2)**2
+    (@integer * (@integer + 1) / 2) **2
   end
 
   def sum_of_squares
     # (N * (N + 1) * (2N + 1)) / 6
-    (@int * (@int + 1) * (2 * @int + 1)) / 6
+    @integer * (@integer + 1) * (2 * @integer + 1) / 6
   end
 
   def difference
