47d862b42a447f8b56798b7c1f06956064b31fbf
leNEKO <le.neko@gmail.com>
2018-10-15 15:27:44 +0200

    saddle: optimized :)

--- rust/saddle-points/src/lib.rs
+++ rust/saddle-points/src/lib.rs
@@ -13,17 +13,15 @@ pub fn find_saddle_points(rows: &[Vec<u64>]) -> Vec<(usize, usize)> {
 
     // storage for already found column mins
     let mut cols_min: Vec<Option<u64>> = vec![None; w];
+    // a closure that return the min value in x column
+    let mut get_min =
+        |x: usize| *cols_min[x].get_or_insert_with(|| rows.iter().map(|row| row[x]).min().unwrap()); // lot happens here
 
     // let's iterate
     for (y, row) in rows.iter().enumerate() {
         let max_in_row = row.iter().max().unwrap();
-        for (x, val) in row.iter().enumerate() {
-            // a closure that return the min value in current column index x
-            let get_min = || rows.iter().map(|row| row[x]).min().unwrap();
-            // search for the min only if not found yet
-            let min_in_col = cols_min[x].get_or_insert_with(get_min);
-
-            if (min_in_col == val) && (max_in_row == val) {
+        for (x, &val) in row.iter().enumerate() {
+            if *max_in_row == val && get_min(x) == val {
                 saddles.push((y, x));
             }
         }
