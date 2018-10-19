aa58bca2edafa8c6371e694486748d9dbe413f35
leNEKO <le.neko@gmail.com>
2018-10-19 09:25:42 +0200

    scrabble-score: corrections

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -1,36 +1,34 @@
-class Scrabble
-  # easy to maintain score configuration
-  SCORE = {
-    'AEIOULNRST' => 1,
-    'DG' => 2,
-    'BCMP' => 3,
-    'FHVWY' => 4,
-    'K' => 5,
-    'JX' => 8,
-    'QZ' => 10
-  }.freeze
-
-  attr_reader :word, :score_map
-
-  # easy to maintain -> easy to use with reduce
-  def init_score_map
-    score_map = {}
-    SCORE.each do |key, value|
-      key.split('').each { |char| score_map[char] = value }
-    end
-    score_map.freeze
+# easy to maintain -> easy to use with function
+def init_score_map(score)
+  score_map = {}
+  score.each do |key, value|
+    key.scan(/\w/).each { |char| score_map[char] = value }
   end
+  score_map.freeze
+end
+
+# easy to maintain score configuration
+SCORE = init_score_map(
+  'AEIOULNRST' => 1,
+  'DG' => 2,
+  'BCMP' => 3,
+  'FHVWY' => 4,
+  'K' => 5,
+  'JX' => 8,
+  'QZ' => 10
+)
+
+class Scrabble
+  attr_reader :chars
 
   def initialize(word)
-    @score_map = init_score_map
-    @word = word.nil? ? '' : word.scan(/\w/).join.upcase
+    @chars = word.to_s.upcase.scan(/\w/)
   end
 
   def score
-    word.split('').reduce(0) { |sum, char| sum + score_map[char] }
+    chars.sum { |char| SCORE[char] }
   end
 
-  # shortcut
   def self.score(word)
     new(word).score
   end
