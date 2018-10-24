6c3b77bbdad69d2b11147ae1ea30e9741aade8a6
Glinglin <le.neko@gmail.com>
2018-10-25 01:27:23 +0200

    robot-name: leaner

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -2,9 +2,7 @@ class Matrix
   attr_reader :rows, :columns
 
   def initialize(str)
-    @rows = str.split(/\n/).collect do |line|
-      line.scan(/\d+/).map(&:to_i)
-    end
+    @rows = str.lines.map { |line| line.split.map(&:to_i) }
     @columns = rows.transpose
   end
 end
