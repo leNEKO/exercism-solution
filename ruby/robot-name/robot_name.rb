ec773c264d2ac1d6dc9f7aa32c957b60b268674d
Glinglin <le.neko@gmail.com>
2018-11-04 22:22:59 +0100

    robot-name: best :)

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -1,2 +1,28 @@
-require_relative 'robot_name_simple.rb'
-# require_relative 'robot_name_complex.rb'
+# Robot: build robot
+class Robot
+  attr_reader :name
+
+  def reset
+    @name = NameGenerator.next
+  end
+  alias initialize reset
+
+  # no need for this with this implementation
+  def self.forget; end
+end
+
+# uniq name generator
+module NameGenerator
+  def self.reset
+    @pool = [*'AA000'..'ZZ999'].shuffle.freeze
+    @cursor = -1
+  end
+
+  # init the factory
+  reset
+
+  def self.next
+    @cursor = (@cursor + 1) % @pool.length
+    @pool[@cursor]
+  end
+end
